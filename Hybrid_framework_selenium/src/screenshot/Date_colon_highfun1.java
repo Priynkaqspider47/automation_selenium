dbbbdb1627a01b3a1b1eee0c22c8ce11b33886f8
VINAY <VINAY@DESKTOP-PCVCUV3>
2023-09-01 17:21:41 +0530

    build b01

--- Hybrid_framework_selenium/src/screenshot/Date_colon_highfun1.java
+++ Hybrid_framework_selenium/src/screenshot/Date_colon_highfun1.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class Date_colon_highfun1 {
 
 	public static void main(String[] args) {
-		
+		//date to string
 			Date d=new Date();
 			System.out.println(d);
 			String d1 = d.toString();
